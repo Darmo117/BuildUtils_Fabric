23249497523136553bf7d0a820d8f19e9de30c51
Darmo117 <darmo117@gmail.com>
2022-06-09 16:49:33 +0200

    Initial commit

---
+++ src/main/java/net/darmo_creations/build_utils/BuildUtils.java
@@ -0,0 +1,18 @@
+package net.darmo_creations.build_utils;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Mod’s main class. Common initializer for both client and server.
+ */
+// TODO mod icon
+public class BuildUtils implements ModInitializer {
+	public static final String MOD_ID = "build_utils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/darmo_creations/build_utils/ClientInitializer.java
@@ -0,0 +1,12 @@
+package net.darmo_creations.build_utils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * Client-side mod initializer.
+ */
+public class ClientInitializer implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+  }
+}
