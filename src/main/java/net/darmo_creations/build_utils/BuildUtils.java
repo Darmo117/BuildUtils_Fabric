960d5e602719f12bfe87b0180b31d7a658d43bd9
Darmo117 <darmo117@gmail.com>
2022-07-03 10:57:22 +0200

    add custom mod icon

--- src/main/java/net/darmo_creations/build_utils/BuildUtils.java
+++ src/main/java/net/darmo_creations/build_utils/BuildUtils.java
@@ -17,7 +17,6 @@ import org.slf4j.LoggerFactory;
 /**
  * Mod’s main class. Common initializer for both client and server.
  */
-// TODO mod icon
 public class BuildUtils implements ModInitializer {
   public static final String MOD_ID = "build_utils";
   public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
